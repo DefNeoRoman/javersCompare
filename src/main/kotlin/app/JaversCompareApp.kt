753542b8b900cd33acbaf8db0d35f77b0918617c
rgaponov <rgaponov@oboz.com>
2020-03-29 10:34:04 +0300

    add RunRestTemplate.kt separately

--- src/main/kotlin/app/JaversCompareApp.kt
+++ src/main/kotlin/app/JaversCompareApp.kt
@@ -8,18 +8,12 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 
-
-
-
 @SpringBootApplication
 @Configuration
 @EnableAutoConfiguration
-
 @ComponentScan("app.*")
 @EntityScan
 open class JaversCompareApp
-
-
 
 fun main(args: Array<String>) {
     SpringApplication.run(JaversCompareApp::class.java, *args)
--- src/test/kotlin/TestContainers.kt
+++ src/test/kotlin/TestContainers.kt
@@ -3,13 +3,10 @@ import app.JaversCompareApp
 import app.dao.Person
 import app.dao.PersonRepository
 import org.junit.jupiter.api.Test
-import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
 
-@RunWith(SpringRunner::class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT, classes = [JaversCompareApp::class])
 class TestContainers {
 
@@ -19,5 +16,8 @@ class TestContainers {
     @Test
     fun testRepo() {
         repository.save(Person(age= 67,name = "dfdf"))
+        val findAll = repository.findAll().count()
+        print(" count of elements - > $findAll ")
+
     }
 }
